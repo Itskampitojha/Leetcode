fe71fcc849c94f7d305160a951efadbb79300a26
Kampit Ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-12-31 23:49:23 +0530

    Create largestsubstring.cpp

---
+++ largestsubstring.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int result = -1;
+        int n      = s.length();
+        
+        for(int i = 0; i < n-1; i++) {
+            for(int j = i+1; j<n; j++) {
+                
+                if(s[i] == s[j]) {
+                    result = max(result, j-i-1);
+                }
+            
+            }
+        }
+        
+        return result;
+    }
+};
